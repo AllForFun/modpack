82a831a4ecd6bf0b83c1f9ae7e02a4dd3e4e0b1c
penne12 <benhaubin@gmail.com>
2014-08-27 17:50:17 -0500

    Manually added potion effects. Added texture

--- java/com/allforfunmc/chocolatestuff/ChocolateBar.java
+++ java/com/allforfunmc/chocolatestuff/ChocolateBar.java
@@ -2,24 +2,30 @@ package com.allforfunmc.chocolatestuff;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+
 public class ChocolateBar extends ItemFood{
-	 
+	@Override
+    public ItemStack onEaten(ItemStack itemStack, World world, EntityPlayer player){
+        --itemStack.stackSize;
+        player.getFoodStats().func_151686_a(this, itemStack);
+        world.playSoundAtEntity(player, "random.burp", 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
+        player.addPotionEffect(new PotionEffect(1, 300 * 20, 3));
+        player.addPotionEffect(new PotionEffect(18, 300 * 20, 3));
+        return itemStack;
+    }
     public ChocolateBar(int healthGain, float SaturationGain, boolean dogsFavorite) {
             super(healthGain, SaturationGain, dogsFavorite);
-            setMaxStackSize(64);
+            setMaxStackSize(1);
             setUnlocalizedName("chocolateBar");
             setTextureName("chocolatestuff:chocolatebar");
-            //No texture yet
             setAlwaysEdible();
             setCreativeTab(CreativeTabs.tabFood);
-            this.setPotionEffect(10, 5, 5, 100);
-            this.setPotionEffect(9, 5, 5, 1);
-            this.setPotionEffect(18, 60, 3, 75);
     }
 }
