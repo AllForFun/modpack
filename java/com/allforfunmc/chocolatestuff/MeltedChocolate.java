a97964850774bb5c46ca8b5477223d3ddbefc0a8
penne12 <benhaubin@gmail.com>
2014-08-27 17:50:55 -0500

    Added in melted chocolate

---
+++ java/com/allforfunmc/chocolatestuff/MeltedChocolate.java
@@ -0,0 +1,13 @@
+package com.allforfunmc.chocolatestuff;
+
+import net.minecraft.item.Item;
+
+public class MeltedChocolate extends Item {
+	public MeltedChocolate(){
+		super();
+		this.setCreativeTab(com.allforfunmc.refineddiamond.Code.AllForFunTab);
+		this.setTextureName("chocolatestuff:meltedChocolate");
+		this.setUnlocalizedName("meltedChocolate");
+		this.setMaxStackSize(1);
+	}
+}
