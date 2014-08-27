9abe79f6f8bbf5363b3359445355bbd011de350b
penne12 <benhaubin@gmail.com>
2014-08-27 17:51:12 -0500

    Recipes and smelting. untested

--- java/com/allforfunmc/chocolatestuff/Code.java
+++ java/com/allforfunmc/chocolatestuff/Code.java
@@ -8,17 +8,26 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Code {
 	public static Item chocolateBar;
+	public static Item meltedChocolate;
 	
 	public static void Blocks() {
 		
 	}	public static void Items() {
 		chocolateBar = new ChocolateBar(5, 5, false);
 		GameRegistry.registerItem(chocolateBar, "chocolateBar");
+		meltedChocolate = new MeltedChocolate();
+		GameRegistry.registerItem(meltedChocolate, "meltedChocolate");
 	}	public static void Crafting() {
-		//Shapeless Recipes
-		
-		//Shaped Recipes
-		
+		GameRegistry.addRecipe(new ItemStack(chocolateBar), new Object[]{
+			"scs",
+			"csc",
+			's', Items.sugar, 'c', meltedChocolate
+		});
+		GameRegistry.addRecipe(new ItemStack(chocolateBar), new Object[]{
+			"scs",
+			"cmc",
+			's', Items.sugar, 'c', meltedChocolate, 'm', Items.milk_bucket}
+		);
 	}	public static void Smelting() {
 		
 	}	public static void Enchantments() {
