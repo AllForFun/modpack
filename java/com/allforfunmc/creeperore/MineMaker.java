0c383a144c88f4068be6da926042b953e13d5e03
penne12 <benhaubin@gmail.com>
2014-09-01 14:38:02 -0500

    Power the minemaker!

--- java/com/allforfunmc/creeperore/MineMaker.java
+++ java/com/allforfunmc/creeperore/MineMaker.java
@@ -22,7 +22,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import com.allforfunmc.creeperore.Mine;
 
 public class MineMaker extends Block{
-    public MineMaker(Material material) {
+	public MineMaker(Material material) {
             super(material);
             setHardness(3F);
             setCreativeTab(com.allforfunmc.refineddiamond.Code.AllForFunTab);
@@ -32,18 +32,50 @@ public class MineMaker extends Block{
     }
     public static HashMap Mines = new HashMap();
     public static Block CreateMine(Block block){
-    		Block mine = new Mine(block);
-    		GameRegistry.registerBlock(mine, block.getLocalizedName() + "Mine");
-    		Mines.put(block, mine);
+    	if(block == Blocks.tnt){
+    		System.out.println("Cannot make a mine out of TNT. Returning null.");
+    		return null;
+    	} else {
+			Block mine = new Mine(block);
+			GameRegistry.registerBlock(mine, block.getLocalizedName() + "Mine");
+			Mines.put(block, mine);
 	    	return mine;
+	    	}
     	}
+    public static HashMap powerhash = new HashMap();
     @Override
-    public boolean onBlockActivated(World p_149727_1_, int p_149727_2_, int p_149727_3_, int p_149727_4_, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_){
-    	try{Block block = Block.getBlockFromItem(player.getCurrentEquippedItem().getItem());
-    	if(Mines.containsKey(block)){
-    		ItemStack mine = new ItemStack((Block) Mines.get(block),player.getCurrentEquippedItem().stackSize);
-        	player.setCurrentItemOrArmor(0, mine);
-    	}}catch(NullPointerException e){}
-    	return true;
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_){
+		String blockkey = String.valueOf(x)+"."+ String.valueOf(y)+"."+String.valueOf(z);
+    	try{
+    		Block block = Block.getBlockFromItem(player.getCurrentEquippedItem().getItem());
+	    	if(Mines.containsKey(block) && powerhash.containsKey(blockkey)){
+	    		if((Integer) powerhash.get(blockkey) >= 5){
+	    			ItemStack mine = new ItemStack((Block) Mines.get(block),player.getCurrentEquippedItem().stackSize);
+	    			player.setCurrentItemOrArmor(0, mine);
+	    			int newpower = (Integer) powerhash.get(blockkey) - 5;
+	    			powerhash.put(blockkey, newpower);
+	    			System.out.println("Power left: " + newpower);
+	    		}
+		    	return true;
+		    	
+	    	} else {
+	    		if(player.getCurrentEquippedItem().getItem() == Code.CreeperPearl){
+		    		int creeperpearls = player.getCurrentEquippedItem().stackSize;
+		        	player.setCurrentItemOrArmor(0, null);
+		    		int power = 0;
+		    		try{power = (Integer) powerhash.get(blockkey) + creeperpearls;}
+		    		catch(NullPointerException e){power = creeperpearls;}
+		    		System.out.println("New Power: "+ power);
+		    		powerhash.put(blockkey,(Integer) power);
+		        	return true;
+	    		} else {
+	    		return false;
+	    		}
+	    	}
+    	} catch(NullPointerException e) {
+	    		System.out.println("Null pointer:");
+	    		System.out.println(e);
+	    		return false;
+    	}
     }
 }
