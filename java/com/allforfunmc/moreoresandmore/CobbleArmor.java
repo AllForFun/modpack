388bb78e1dfc577a960a7412b65a35b40e9cb3a7
ndh175 <ndh175@gmail.com>
2014-09-03 00:02:49 -0400

    Cobble Armor

---
+++ java/com/allforfunmc/moreoresandmore/CobbleArmor.java
@@ -0,0 +1,56 @@
+package com.allforfunmc.moreoresandmore;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+public class CobbleArmor extends ItemArmor{
+
+	private static Object shiftedIndex;
+
+	public CobbleArmor(ArmorMaterial material, int id, int slot) {
+		super(material, id, slot);
+		
+		this.setCreativeTab(com.allforfunmc.refineddiamond.Code.AllForFunTab);
+		
+		if(slot == 0) {
+			this.setTextureName(Main.modid + ":cobble_helm");
+		}else if(slot == 1) {
+			this.setTextureName(Main.modid + ":cobble_chest");
+		}else if(slot == 2) {
+			this.setTextureName(Main.modid + ":cobble_legs");
+		}else if(slot == 3) {
+			this.setTextureName(Main.modid + ":cobble_boots");
+		}
+		
+
+	}
+	
+	/* @Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemstack) {
+			if (player.getCurrentArmor(3) != null && player.getCurrentArmor(2) != null && player.getCurrentArmor(1) != null && player.getCurrentArmor(1) != null) {
+					ItemStack helmet = player.getCurrentArmor(3);
+					ItemStack chest = player.getCurrentArmor(2);
+					ItemStack legs = player.getCurrentArmor(1);
+					ItemStack boots = player.getCurrentArmor(0);
+				if (helmet.getItem() == Main.armorCobbleHelm && chest.getItem() == Main.armorCobbleChest && legs.getItem() == Main.armorCobbleLegs && boots.getItem() == Main.armorCobbleBoots) {
+					player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.getId(), 100));
+				}
+			}
+		} */
+	
+		public String getArmorTexture(ItemStack itemstack, Entity entity, int slot, String type) {
+			if (itemstack.getItem() == Main.armorCobbleHelm || itemstack.getItem() == Main.armorCobbleChest || itemstack.getItem() == Main.armorCobbleBoots) {
+					return Main.modid + ":textures/models/cobble_layer_1.png";
+		} else if (itemstack.getItem() == Main.armorCobbleLegs) {
+					return Main.modid + ":textures/models/cobble_layer_2.png";
+		} else {
+			return null;
+		}
+	}
+}
--- java/com/allforfunmc/moreoresandmore/Main.java
+++ java/com/allforfunmc/moreoresandmore/Main.java
@@ -6,10 +6,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.EnumHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -23,39 +26,50 @@ public class Main {
 	public static final String modid = "moreoresandmore";
 	public static final String version = "1.0.0";
 	
+	public static int armorCobbleHelmID;
+	public static int armorCobbleChestID;
+	public static int armorCobbleLegsID;
+	public static int armorCobbleBootsID;
+	
+	public static Item armorCobbleHelm;
+	public static Item armorCobbleChest;
+	public static Item armorCobbleLegs;
+	public static Item armorCobbleBoots;
+	
+	public static ArmorMaterial CobbleArmorMaterial = EnumHelper.addArmorMaterial("CobbleArmor", 15, new int[] {2, 6, 5, 2}, 9);
+	
 	public static Block blockCompressedCobble;
-	
-	public static String bootName = "Iron Boots"; 
-	
-	public Main(World world, EntityPlayer player, ItemStack itemStack) {
-
-		if (player.getCurrentArmor(3) != null && player.getCurrentArmor(2) != null && player.getCurrentArmor(1) != null && player.getCurrentArmor(0) != null) {
-			ItemStack helmet = player.getCurrentArmor(3);
-			ItemStack plate = player.getCurrentArmor(2);
-			ItemStack legs = player.getCurrentArmor(1);
-			ItemStack boots = player.getCurrentArmor(0);
-			
-		if (helmet.getItem() == Items.iron_helmet && plate.getItem() == Items.iron_chestplate && legs.getItem() == Items.iron_leggings && boots.getItem() == Items.iron_boots)
-			{
-			//player.fallDistance = 0.0F;
-			player.addPotionEffect(new PotionEffect(Potion.jump.getId(), 200, 10));
-			player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 200, 10));
-			}
-			}
-		} 
 
 	@EventHandler
 	public void PreInit(FMLPreInitializationEvent preEvent){
-<<<<<<< HEAD
+			/* <<<<<<< HEAD
 	
 
-=======
-		//new ores. test
->>>>>>> origin/master
+			=======
+			//new ores. test
+			>>>>>>> origin/master */
 		
 		//blocks
 		blockCompressedCobble = new CompressedCobble(Material.rock).setBlockName("CompressedCobble").setHardness(5.0F).setResistance(2000.0F);
 		GameRegistry.registerBlock(blockCompressedCobble, "CompressedCobble");
+		
+		//items
+		
+		
+		
+		//armor
+		armorCobbleHelm = new CobbleArmor(CobbleArmorMaterial, armorCobbleHelmID, 0).setUnlocalizedName("CobbleHelm");
+		GameRegistry.registerItem(armorCobbleHelm, "CobbleHelm");
+		
+		armorCobbleChest = new CobbleArmor(CobbleArmorMaterial, armorCobbleChestID, 1).setUnlocalizedName("CobbleChest");
+		GameRegistry.registerItem(armorCobbleChest, "CobbleChest");
+		
+		armorCobbleLegs = new CobbleArmor(CobbleArmorMaterial, armorCobbleLegsID, 2).setUnlocalizedName("CobbleLegs");
+		GameRegistry.registerItem(armorCobbleLegs, "CobbleLegs");
+		
+		armorCobbleBoots = new CobbleArmor(CobbleArmorMaterial, armorCobbleBootsID, 3).setUnlocalizedName("CobbleBoots");
+		GameRegistry.registerItem(armorCobbleBoots, "CobbleBoots");
+		
 		
 	}
 	
@@ -64,6 +78,11 @@ public class Main {
 		
 		//crafting
 		GameRegistry.addShapedRecipe(new ItemStack(blockCompressedCobble, 1), new Object[]{"XXX", "XXX", "XXX", 'X', Blocks.cobblestone});
+		
+		GameRegistry.addShapedRecipe(new ItemStack(armorCobbleHelm, 1), new Object[]{"XXX", "X X", 'X', blockCompressedCobble});
+		GameRegistry.addShapedRecipe(new ItemStack(armorCobbleChest, 1), new Object[]{"X X", "XXX", "XXX", 'X', blockCompressedCobble});
+		GameRegistry.addShapedRecipe(new ItemStack(armorCobbleLegs, 1), new Object[]{"XXX", "X X", "X X", 'X', blockCompressedCobble});
+		GameRegistry.addShapedRecipe(new ItemStack(armorCobbleBoots, 1), new Object[]{"X X", "X X", 'X', blockCompressedCobble});
 		
 	}
 	
