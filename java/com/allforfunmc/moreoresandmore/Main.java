75895c09a02de0b16960c698cb16847d6dbe646e
penne12 <penne12@allforfunmc.com>
2014-09-04 08:04:41 -0500

    Compressed cobble more

--- java/com/allforfunmc/moreoresandmore/Main.java
+++ java/com/allforfunmc/moreoresandmore/Main.java
@@ -87,7 +87,7 @@ public class Main {
 	@EventHandler
 	public void PostInit(FMLPostInitializationEvent postEvent){
 		/*Args: int maxX, int maxZ, int maxVeinSize, int chancesToSpawn, int minY, int maxY*/
-		EasyOre.EasyGenerator(blockCompressedCobble, 10, 10, 25, 25, 1, 35,24);
+		EasyOre.EasyGenerator(blockCompressedCobble, 2, 2, 100, 500, 1, 35,24);
 		//items
 	}
 }
