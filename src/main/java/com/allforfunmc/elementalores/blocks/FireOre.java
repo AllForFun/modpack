fa6418a7ad088ec0c892a9c4d5ac6601b5f1e40f
ess090502 <ess@allforfunmc.com>
2014-09-28 16:09:33 -0500

    Deleted Stuff we dont need

--- src/main/java/com/allforfunmc/elementalores/blocks/FireOre.java
+++ src/main/java/com/allforfunmc/elementalores/blocks/FireOre.java
@@ -2,6 +2,7 @@ package com.elementalores.blocks;
 
 import com.elementalores.elementaloresmod.Blocks;
 import com.elementalores.elementaloresmod.ElementalOresMod;
+import com.elementalores.elementaloresmod.Items;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -12,7 +13,7 @@ public class FireOre extends  Block{
 	public FireOre (Material mat){
 		super(mat);
 		setBlockName("fire_ore");
-		setCreativeTab(Blocks.moditemsTab);
+		setCreativeTab(Items.moditemsTab);
 		this.setHardness(6f);
 		this.setHarvestLevel("pickaxe", 3);
 		this.setBlockTextureName("sleshymod:fire_ore");
