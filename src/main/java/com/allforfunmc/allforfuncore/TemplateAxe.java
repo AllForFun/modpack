569d41c7ecbad4c6d54523c236a2ea42d5c62a81
penne12 <penne12@allforfunmc.com>
2014-09-29 15:16:17 -0500

    Template Axe

---
+++ src/main/java/com/allforfunmc/allforfuncore/TemplateAxe.java
@@ -0,0 +1,11 @@
+package com.allforfunmc.allforfuncore;
+
+import net.minecraft.item.ItemAxe;
+
+public class TemplateAxe extends ItemAxe{
+	public TemplateAxe(ToolMaterial mat){
+		super(mat);
+		this.setMaxStackSize(1);
+		this.setCreativeTab(Core.AllForFunTools);
+	}
+}
