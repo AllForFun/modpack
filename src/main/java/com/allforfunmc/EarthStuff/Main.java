fce39dcc29dfeea35ebe8c640d332027a10aed72
penne12 <penne12@allforfunmc.com>
2014-09-29 15:16:08 -0500

    #6 Earth Ore

--- src/main/java/com/allforfunmc/EarthStuff/Main.java
+++ src/main/java/com/allforfunmc/EarthStuff/Main.java
@@ -5,6 +5,7 @@ import net.minecraft.block.BlockStone;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemSpade;
@@ -12,6 +13,7 @@ import net.minecraft.item.ItemSword;
 import net.minecraftforge.common.util.EnumHelper;
 
 import com.allforfunmc.allforfuncore.Core;
+import com.allforfunmc.allforfuncore.TemplateAxe;
 import com.allforfunmc.allforfuncore.TemplatePick;
 
 import cpw.mods.fml.common.Mod;
@@ -19,6 +21,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod (modid="EarthStuff", name="AllForFun's EarthStuff", version="1")
 public class Main {
@@ -29,6 +32,10 @@ public class Main {
 	@EventHandler()
 	public void load(FMLInitializationEvent event) {
 		proxy.registerRenderers();
+		GameRegistry.registerItem(EarthGem, "earth_gem");
+		GameRegistry.registerItem(EarthSword, "earth_sword");
+		GameRegistry.registerItem(EarthPickaxe, "earth_pickaxe");
+		GameRegistry.registerItem(EarthHoe, "earth_hoe");
 	}
 	
 	public static Item EarthGem = (new Item()).setUnlocalizedName("earth_gem").setCreativeTab(Core.AllForFunItems).setTextureName("sleshymod:earth_gem");
@@ -38,6 +45,7 @@ public class Main {
 	public static Item EarthSword = (new ItemSword(EarthMat)).setMaxStackSize(1).setUnlocalizedName("earth_sword").setCreativeTab(Core.AllForFunTools).setTextureName("sleshymod:earth_sword");
 	public static Item EarthPickaxe = (new TemplatePick(EarthMat)).setUnlocalizedName("earth_pick").setTextureName("sleshymod:earth_pick");
 	public static Item EarthHoe = (new ItemHoe(EarthMat)).setMaxStackSize(1).setUnlocalizedName("earth_hoe").setCreativeTab(Core.AllForFunTools).setTextureName("sleshymod:earth_hoe");
+	public static Item EarthAxe = (new TemplateAxe(EarthMat)).setMaxStackSize(1).setUnlocalizedName("earth_axe").setCreativeTab(Core.AllForFunTools).setTextureName("sleshymod:earth_axe");
 	
 	public static Block EarthBlock = new EarthBlock(Material.ground);
 	public static Block EarthOre = new EarthOre(Material.rock);
