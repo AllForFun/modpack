c4e1b48eafece110ab92324a3caf324d685b245b
penne12 <penne12@allforfunmc.com>
2014-09-30 20:08:57 -0500

    More #6

--- src/main/java/com/allforfunmc/fire/Fire.java
+++ src/main/java/com/allforfunmc/fire/Fire.java
@@ -3,6 +3,7 @@ package com.allforfunmc.fire;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemSword;
 import net.minecraftforge.common.util.EnumHelper;
 
 import com.allforfunmc.allforfuncore.Core;
@@ -32,13 +33,15 @@ public class Fire {
 	public static Item FireAxe = (new TemplateAxe(FireMaterial)).setUnlocalizedName("fireaxe").setTextureName("sleshymod:fire_axe");
 	public static Item FireHoe = (new ItemHoe(FireMaterial)).setMaxStackSize(1).setUnlocalizedName("fire_hoe").setCreativeTab(Core.AllForFunTools).setTextureName("sleshymod:fire_hoe");
 	public static Item FirePick = (new TemplatePick(FireMaterial));
+	public static Item FireSword = (new ItemSword(FireMaterial)).setMaxStackSize(1).setUnlocalizedName("fire_shovel").setCreativeTab(Core.AllForFunTools).setTextureName("fire_shovel");
 	
 	@EventHandler()
 	public void load(FMLInitializationEvent event) {
 		proxy.registerRenderers();
 		GameRegistry.registerItem(FireCrystal, "fire_crystal");
-		MassRegister.Items(new Item[]{FireAxe,FireHoe,FirePick,FireCrystal}, new String[]{"fire_axe","fire_hoe","fire_pick","fire_crystal"}, "fire");
-	}
+		MassRegister.Items(new Item[]{FireAxe,FireHoe,FirePick,FireCrystal,FireSword}, new String[]{"fire_axe","fire_hoe","fire_pick","fire_crystal","fire_shovel"}, "fire");
+		
+	}	
 	@EventHandler()
 	public void postInit(FMLInitializationEvent event){
 		Crafting.smartRegister("Axe", FireCrystal, FireAxe);
