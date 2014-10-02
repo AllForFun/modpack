2baa51f3534c59a6708ef9307b133f58465bda0e
penne12 <penne12@allforfunmc.com>
2014-10-01 20:29:26 -0500

    Turns block destroyed into ice if not ice

--- src/main/java/com/allforfunmc/FrozenMod/IcePick.java
+++ src/main/java/com/allforfunmc/FrozenMod/IcePick.java
@@ -1,11 +1,15 @@
 package com.allforfunmc.FrozenMod;
 
-import com.elementalores.elementaloresmod.BlocksAndItems;
-import com.elementalores.elementaloresmod.ElementalOresMod;
+import com.allforfunmc.allforfuncore.Core;
 
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPickaxe;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class IcePick extends ItemPickaxe{
 	
@@ -13,8 +17,20 @@ public class IcePick extends ItemPickaxe{
 		super(mat);
 		this.setMaxStackSize(1);
 		setUnlocalizedName("ice_pick");
-		setCreativeTab(BlocksAndItems.moditemsTab);
+		setCreativeTab(Core.AllForFunTools);
 		this.setTextureName("sleshymod:ice_pick");
 	}
+	@Override
+	public boolean onBlockDestroyed(ItemStack p_150894_1_, World world, Block block, int x, int y, int z, EntityLivingBase p_150894_7_)
+    {
+        if ((double)block.getBlockHardness(world, x, y, z) != 0.0D)
+        {
+            p_150894_1_.damageItem(1, p_150894_7_);
+        }
+        if (!Block.isEqualTo(block, Blocks.ice))  {
+        	world.setBlock(x, y, z, Blocks.ice);
+        }
+        return true;
+    }
 
 }
