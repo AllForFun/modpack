f20fc60bcfe60838bffa7eb82fe3d08c33e743d7
penne12 <penne12@allforfunmc.com>
2014-10-05 19:20:45 -0500

    new chocolate bars 0 = milk, 1 = white, 2 = dark

--- src/main/java/com/allforfunmc/chocolatestuff/ChocolateBar.java
+++ src/main/java/com/allforfunmc/chocolatestuff/ChocolateBar.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
@@ -16,14 +17,23 @@ public class ChocolateBar extends ItemFood{
         --itemStack.stackSize;
         player.getFoodStats().func_151686_a(this, itemStack);
         world.playSoundAtEntity(player, "random.burp", 0.5F, world.rand.nextFloat() * 0.1F + 0.9F);
-        player.addPotionEffect(new PotionEffect(1, 300 * 20, 3));
-        player.addPotionEffect(new PotionEffect(18, 300 * 20, 3));
-        return itemStack;
+        switch (itemStack.getItemDamage()){
+        case 0:
+            player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 300 * 20, 3));
+            player.addPotionEffect(new PotionEffect(Potion.weakness.getId(), 300 * 20, 3));
+        case 1:
+            player.addPotionEffect(new PotionEffect(Potion.nightVision.getId(), 300 * 20, 1));
+            player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.getId(), 300 * 20, 3));
+        case 2:
+        	player.addPotionEffect(new PotionEffect(Potion.damageBoost.getId(), 300 * 20, 3));
+        	player.addPotionEffect(new PotionEffect(Potion.confusion.getId(), 300 * 20, 3));
+        }
+        return itemStack; 
     }
-    public ChocolateBar(int healthGain, float SaturationGain, boolean dogsFavorite) {
-            super(healthGain, SaturationGain, dogsFavorite);
+    public ChocolateBar() {
+            super(5, 0, false);
             setMaxStackSize(1);
-            setUnlocalizedName("chocolateBar");
+            setUnlocalizedName("chocolatebar.unknown");
             setTextureName("chocolatestuff:chocolatebar");
             setAlwaysEdible();
             setCreativeTab(CreativeTabs.tabFood);
