089b974f6cd947f7247f87bbaa611a36ac4d3979
penne12 <penne12@allforfunmc.com>
2014-10-09 18:40:49 -0500

    Template Block

---
+++ src/main/java/com/allforfunmc/allforfuncore/TemplateBlock.java
@@ -0,0 +1,14 @@
+package com.allforfunmc.allforfuncore;
+
+import com.allforfunmc.allforfuncore.Core;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class TemplateBlock extends  Block{
+	public TemplateBlock(Material mat, String Name){
+		super(mat);
+		this.setCreativeTab(Core.AllForFunBlocks);
+		this.setBlockName(Name);
+	}
+}
