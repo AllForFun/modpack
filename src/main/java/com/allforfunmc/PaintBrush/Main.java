7c0c02ca190901b3b4ca4938726515b76b2996ea
penne12 <penne12@allforfunmc.com>
2014-10-12 10:08:39 -0500

    Testing

--- src/main/java/com/allforfunmc/PaintBrush/Main.java
+++ src/main/java/com/allforfunmc/PaintBrush/Main.java
@@ -9,17 +9,22 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemTool;
 
-//@Mod (modid="PaintingMod", name="AllForFun's Painting Mod", version="1")
+@Mod (modid="PaintingMod", name="AllForFun's Painting Mod", version="1")
 public class Main {
 	@Instance (value="GenericModID")
 	public static PaintingMod instance;
+	
+	public static Item PaintBrush = new Paintbrush();
+	
 	public class PaintingMod {
 		
 		@EventHandler()
 		public void load(FMLInitializationEvent event) {
-			
+			GameRegistry.registerItem(PaintBrush, "paint_brush");
 		}
 		@EventHandler()
 		public void postInit(FMLInitializationEvent event){
--- src/main/java/com/allforfunmc/PaintBrush/Paintbrush.java
+++ src/main/java/com/allforfunmc/PaintBrush/Paintbrush.java
@@ -1,7 +1,12 @@
 package com.allforfunmc.PaintBrush;
 
+import java.util.ArrayList;
+
+import com.allforfunmc.allforfuncore.Core;
+
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -9,14 +14,29 @@ import net.minecraftforge.common.util.ForgeDirection;
 
 public class Paintbrush extends Item{
 	public Paintbrush(){
-		
+		this.setCreativeTab(Core.AllForFunTools);
+		this.setUnlocalizedName("paintbrush");
 	}
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
     {
-		int x = (int) player.getLookVec().xCoord;
-		int y = (int) player.getLookVec().yCoord;
-		int z = (int) player.getLookVec().zCoord;
-		
-        return stack;
+		if (!world.isRemote){
+			int x = (int) player.getLookVec().xCoord;
+			int y = (int) player.getLookVec().yCoord;
+			int z = (int) player.getLookVec().zCoord;
+			
+			Block block = world.getBlock(x, y, z);
+			int meta = world.getBlockMetadata(x, y, z);
+			
+			if(block.equals(Blocks.wool)){
+				meta++;
+			}
+			
+			world.setBlock(x, y, z, block);
+			world.setBlock(x, y, z, block, meta, 2);
+			
+	        return stack;
+		} else {
+			return stack;
+		}
     }
 }
