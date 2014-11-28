aab4dd61c86c571b66c5376b9f70990840fda2a6
Ben <benhaubin@gmail.com>
2014-11-28 10:48:20 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 cd ${PWD}
-mkdir .temp
-cd ./.temp
-curl -L -o mcforge.zip http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.1.1219/forge-1.7.10-10.13.1.1219-src.zip
-unzip mcforge.zip -d ./mcforge
-cd ..
-mv ./.temp/mcforge/eclipse ./
-mv build.gradle ./.temp/build.gradle
-mv ./.temp/mcforge/build.gradle ./
-./gradlew setupdecompworkspace —-refresh-dependencies
-./gradlew eclipse
-rm build.gradle
-mv ./.temp/build.gradle build.gradle
-rm -rf ./.temp
+
+echo > apikey.txt
+./gradlew setupdecompworkspace --refresh-dependencies;
+read -p "Do you want to use Eclipse? " yn
+    case $yn in
+        [Yy]* ) ./gradlew eclipse;
+        [Nn]* ) ;
+        * );
+    esac
+
