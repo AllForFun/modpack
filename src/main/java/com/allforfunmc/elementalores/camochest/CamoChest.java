d317d0dbfffb497396db8624457b41d50540b59f
Ben <benhaubin@gmail.com>
2014-11-27 20:30:10 -0600

    Fixes #37, although breaking the block is bad.

--- src/main/java/com/allforfunmc/elementalores/camochest/CamoChest.java
+++ src/main/java/com/allforfunmc/elementalores/camochest/CamoChest.java
@@ -111,51 +111,6 @@ public class CamoChest extends BlockChest{
         }
     }
 	
-	public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_)
-    {
-        TileEntityCamoChest tileentitychest = (TileEntityCamoChest)p_149749_1_.getTileEntity(p_149749_2_, p_149749_3_, p_149749_4_);
-
-        if (tileentitychest != null)
-        {
-            for (int i1 = 0; i1 < tileentitychest.getSizeInventory(); ++i1)
-            {
-                ItemStack itemstack = tileentitychest.getStackInSlot(i1);
-
-                if (itemstack != null)
-                {
-                    float f = this.field_149955_b.nextFloat() * 0.8F + 0.1F;
-                    float f1 = this.field_149955_b.nextFloat() * 0.8F + 0.1F;
-                    EntityItem entityitem;
-
-                    for (float f2 = this.field_149955_b.nextFloat() * 0.8F + 0.1F; itemstack.stackSize > 0; p_149749_1_.spawnEntityInWorld(entityitem))
-                    {
-                        int j1 = this.field_149955_b.nextInt(21) + 10;
-
-                        if (j1 > itemstack.stackSize)
-                        {
-                            j1 = itemstack.stackSize;
-                        }
-
-                        itemstack.stackSize -= j1;
-                        entityitem = new EntityItem(p_149749_1_, (double)((float)p_149749_2_ + f), (double)((float)p_149749_3_ + f1), (double)((float)p_149749_4_ + f2), new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
-                        float f3 = 0.05F;
-                        entityitem.motionX = (double)((float)this.field_149955_b.nextGaussian() * f3);
-                        entityitem.motionY = (double)((float)this.field_149955_b.nextGaussian() * f3 + 0.2F);
-                        entityitem.motionZ = (double)((float)this.field_149955_b.nextGaussian() * f3);
-
-                        if (itemstack.hasTagCompound())
-                        {
-                            entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
-                        }
-                    }
-                }
-            }
-
-            p_149749_1_.func_147453_f(p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_);
-        }
-
-        super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
-    }
 	
 	public IInventory func_149951_m(World p_149951_1_, int p_149951_2_, int p_149951_3_, int p_149951_4_)
     {
