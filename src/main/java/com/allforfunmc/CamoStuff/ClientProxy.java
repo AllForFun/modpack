5da6999bd5715ca6e96aa7ee9ea5782ed7ba9f25
Ben! <penne12@allforfunmc.com>
2014-11-28 21:35:48 -0600

    Revert "revert it"
    This reverts commit 566081c3760a98700e6fbabb9fd69e9cb38a3a59.

--- src/main/java/com/allforfunmc/CamoStuff/ClientProxy.java
+++ src/main/java/com/allforfunmc/CamoStuff/ClientProxy.java
@@ -3,6 +3,7 @@ package com.allforfunmc.CamoStuff;
 
 import com.allforfunmc.CamoStuff.camochest.CamoChestRenderer;
 import com.allforfunmc.CamoStuff.camochest.ItemRendererCamoChest;
+import com.allforfunmc.CamoStuff.camochest.MainRegistry;
 import com.allforfunmc.CamoStuff.camochest.TileEntityCamoChest;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.item.Item;
@@ -13,7 +14,7 @@ public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers() {
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityCamoChest.class, new CamoChestRenderer());
-        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(Main.CamoChest), new ItemRendererCamoChest());
+        MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(MainRegistry.camochest), new ItemRendererCamoChest());
     }
 
 }
