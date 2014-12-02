9f2dbc4a3b18f244cf572f43c30d8725209b5d4d
Ben! <penne12@allforfunmc.com>
2014-12-01 18:42:30 -0600

    Some work

--- src/main/java/com/allforfunmc/CamoStuff/CamoBlockEntity.java
+++ src/main/java/com/allforfunmc/CamoStuff/CamoBlockEntity.java
@@ -13,12 +13,16 @@ public class CamoBlockEntity extends TileEntity{
 	public void readFromNBT(NBTTagCompound nbt)
 	{
 		block = Block.getBlockById(nbt.getInteger("Block"));
+		meta = nbt.getInteger("Meta");
 	}
 
 	@Override
 	public void writeToNBT(NBTTagCompound nbt)
 	{
 		nbt.setInteger("Block", Block.getIdFromBlock(block));
+		nbt.setInteger("Meta", meta);
 	}
+
+
 
 }
